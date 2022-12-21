7794482b64ad7bb7c12e5e3be230de4fa48b874a
bpena707 <bpena.610@gmail.com>
2022-12-20 21:46:01 -0800

    css styles
    added the css styles to the webpage.

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -2,7 +2,7 @@
 function Header(){
     return(
         <header className="header">
-            <img src='./assets/boredApe.jpeg' alt="Bored Ape Image" />
+            <img src='./assets/troll-face.png' alt="Troll Face" />
             <h3>Meme Generator</h3>
         </header>
        
--- src/Components/Meme.tsx
+++ src/Components/Meme.tsx
@@ -1,6 +1,35 @@
+import { useState } from "react"
+
+
+
 function Meme(){
+
+    
+
     return(
-        <h1>Meme Page</h1>
+        <main>
+            <div className="form">
+                <input 
+                    type="text" 
+                    placeholder="Top Text..."
+                    className="formInput"
+                    name="topText"
+                />
+                <input 
+                    type="text" 
+                    placeholder="Bottom Text..."
+                    className="formInput"
+                    name="bottomText"
+                />
+                <button className="memeButton">Get new meme image 🖼️ </button>
+            </div>
+            <div className="meme">
+                <img src="http://i.imgflip.com/1bij.jpg" alt="" className="memeImage"/>
+                <h2 className="meme-text top">top</h2>
+                <h2 className="meme-text bottom">bottom</h2>
+            </div>
+            
+        </main>
     )
 }
 
