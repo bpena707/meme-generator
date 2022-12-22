f406a114034d55948bef24a940e56707dfc4b92a
bpena707 <bpena.610@gmail.com>
2022-12-21 22:20:07 -0800

    Update Meme.tsx
    included different functions to handle different events that update state and implemented them in place of the hard coded values

--- src/Components/Meme.tsx
+++ src/Components/Meme.tsx
@@ -4,47 +4,85 @@ import { useEffect, useState } from "react"
 
 function Meme(){
 
+    // state holds data in the component as an object so that these 
+    //values can be re-rendered when the user causes change 
+    //including new text or getting a new image
     const [meme, setMeme] = useState({
-        topText: "topText",
-        bottomText: "bottomText",
-        img: "http://i.imgflip.com/1bij.jpg"
+        topText: "",
+        bottomText: "",
+        randomImage: "http://i.imgflip.com/1bij.jpg"
     })
 
-    const [memeImage, setMemeImage] = useState([])
+    //state for image is stored as an array since the data
+    //is also in an array.
+    const [allMeme, setAllMeme] = useState([])
 
+    //useEffect to take care of side effects of fetcing images.
+    //prevents function from infinite loop of grabbing the images over
+    //and over again. 
     useEffect(() => {
-        fetch("https://api.imgflip.com/get_memes")
-            .then(res => res.json())
-            .then(data => setMemeImage(data.data.memes))
+        async function getMemes() {
+            const res = await fetch("https://api.imgflip.com/get_memes")
+            const data = await res.json()
+            setAllMeme(data.data.memes)
+        }
+        getMemes()
     }, [])
 
-    console.log(memeImage)
+
+    //function grabs a random image from the data and renders 
+    //it using state to update the chage 
+    function getMemeImages(){
+        const randomNumber = Math.floor(Math.random() * allMeme.length)
+        const url = allMeme[randomNumber].url
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            randomImage: url
+        }))
+    }
+
+    //set the state targeting the event to update the name value key
+    // so that the state can change the text while user is typing up thier meme
+    function handleChange(event){
+        const {name, value} = event.target
+        setMeme(prevMeme => ({
+            ...prevMeme,
+            [name]: value
+        }))
+
+    }
 
     
 
     return(
         <main>
+            {/* form where the text goes and is updated on state using 
+            different function to handle state and change */}
             <div className="form">
                 <input 
                     type="text" 
                     placeholder="Top Text..."
                     className="formInput"
                     name="topText"
-                    
+                    onChange={handleChange}
+                    value={meme.topText}
                 />
                 <input 
                     type="text" 
                     placeholder="Bottom Text..."
                     className="formInput"
                     name="bottomText"
-                    
+                    onChange={handleChange}
+                    value={meme.bottomText}
                 />
-                <button className="memeButton">Get new meme image 🖼️ </button>
+                <button className="memeButton" onClick={getMemeImages}>Get new meme image 🖼️ </button>
             </div>
+            {/* this is the meme gereation output where the text appears on top 
+            of the randomly rendered image */}
             <div className="meme">
-                <img src="http://i.imgflip.com/1bij.jpg" alt="" className="memeImage"/>
-                <h2 className="meme-text top">top</h2>
-                <h2 className="meme-text bottom">bottom</h2>
+                <img src={meme.randomImage} alt="" className="memeImage"/>
+                <h2 className="meme-text top">{meme.topText}</h2>
+                <h2 className="meme-text bottom">{meme.bottomText}</h2>
             </div>
             
         </main>
